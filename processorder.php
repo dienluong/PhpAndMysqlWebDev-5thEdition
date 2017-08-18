663865f4c0b3b6f1f64189476f258adf592227d2
Dien Luong <dysonlu@gmail.com>
2017-08-18 18:12:57 -0400

    htmlspecialchars

--- processorder.php
+++ processorder.php
@@ -7,9 +7,15 @@
 <h1>Bob's Auto Parts</h1>
 <h2>Order Results</h2>
 <?php
+$tireqty = $_POST['tireqty'];
+$oilqty = $_POST['oilqty'];
+$sparkqty = $_POST['sparkqty'];
 
-echo '<p>Order processed.</p>';
-
+echo '<p>You ordered:</p>';
+echo htmlspecialchars($tireqty).' tires<br />';
+echo htmlspecialchars($oilqty).' bottles of oil<br />';
+echo htmlspecialchars($sparkqty).' spark plugs<br />';
+echo "<p>Order processed at ".date("H:i, jS F Y")."</p>";
 ?>
 </body>
 </html>
