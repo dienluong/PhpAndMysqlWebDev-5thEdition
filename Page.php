d21bfab2be1e5c88df0d737d43b9246caf85d5f6
Dien Luong <dysonlu@gmail.com>
2018-01-30 00:18:45 -0500

    Chapter 6 continued

--- Page.php
+++ Page.php
@@ -19,14 +19,82 @@ class Page
 
     public function display() {
         echo "<html>\n<head>\n";
-        $this -> DisplayTitle();
-        $this -> DisplayKeywords();
-        $this -> DisplayStyles();
+        $this -> displayTitle();
+        $this -> displayKeywords();
+        $this -> displayStyles();
         echo "</head>\n<body>\n";
-        $this -> DisplayHeader();
-        $this -> DisplayMenu($this->buttons);
+        $this -> displayHeader();
+        $this -> displayMenu($this->buttons);
         echo $this->content;
-        $this -> DisplayFooter();
+        $this -> displayFooter();
         echo "</body>\n</html>\n";
+    }
+
+    public function isURLCurrentPage($url) {
+        return (strpos($_SERVER['PHP_SELF'], $url) === FALSE ?: TRUE);
+    }
+
+    public function displayTitle() {
+        echo "<title>$this->title</title>";
+    }
+
+    public function displayKeywords() {
+        echo "<meta name='keywords' content='$this->keywords'/>"
+    }
+
+    public function displayStyles() {
+        ?>
+        <link rel="stylesheet" href="styles.css">
+        <?php
+    }
+
+    public function displayMenu($buttons) {
+        echo '<nav>';
+        foreach ($buttons as $name => $url) {
+            $this->displayButton($name, $url, !$this->isURLCurrentPage($url));
+        }
+        echo '</nav>\n';
+    }
+
+    public function displayButton($name, $url, $active=TRUE) {
+        if ($active) {
+            ?>
+            <div class="menuitem">
+                <a href="<?=$url?>">
+                    <img src="s-logo.gif" alt="" height="20" width="20" />
+                    <span class="menutext"><?=$name?></span>
+                </a>
+            </div>
+            <?php
+        }
+        else {
+            ?>
+            <div class="menuitem">
+                <img src="side-logo.gif">
+                <span class="menutext"><?=$name?></span>
+            </div>
+            <?php
+        }
+    }
+
+    public function displayHeader() {
+        ?>
+        <!-- page header -->
+        <header>
+            <img src="logo.gif" alt="TLA logo" height="70" width="70" />
+            <h1>TLA Consulting</h1>
+        </header>
+        <?php
+    }
+
+    public function displayFooter() {
+        ?>
+        <!-- page footer -->
+        <footer>
+            <p>&copy; TLA Consulting Pty Ltd.<br />
+                Please see our
+                <a href="legal.php">legal information page</a>.</p>
+        </footer>
+        <?php
     }
 }
--- processfeedback.php
+++ processfeedback.php
@@ -6,24 +6,31 @@
  * Time: 17:26
  */
 
+$sent = FALSE;
+$errorMsg = '<p>Cannot submit: fields cannot be left blank.</p>';
+
 $name = trim($_POST['yourName']) ?: NULL;
 $email = trim($_POST['yourEmail']) ?: NULL;
 $feedback = trim($_POST['yourFeedback']) ?: NULL;
-$sent = FALSE;
 if (isset($name, $email, $feedback)) {
-    $fromHeader = "From: mamp@example.com";
-    $toAddress = "dysonlu0@gmail.com";
+    $fromHeader = 'From: mamp@example.com';
+    $toAddress = 'dysonlu0@gmail.com';
     $emailSubject = "Feedback from $email";
     // remove \r\n for security reasons.
     $emailContent = str_replace("\r\n", "","Customer: \"$name\"\nEmail: \"$email\"\nComment: \"$feedback\"\n");
     if ($email) {
-        $emailExploded = explode('@', $email);
-        if ($emailExploded[1] === 'bigcustomer.com') {
-            $emailSubject = "IMPORTANT: Feedback from $email";
+        if (preg_match('/^[a-z0-9_\-\.]+@[a-z0-9\-]+\.[a-z0-9\-\.]+$/i', $email) === 0) {
+            $email = NULL;
+            $errorMsg = '<p>Invalid email entered!</p>';
         }
-    }
-    if (mail($toAddress, $emailSubject, $emailContent, $fromHeader)) {
-        $sent = TRUE;
+        else {
+            if (preg_match('/(?:@bigcustomer\.com)$/i', $email)) {
+                $emailSubject = "IMPORTANT: Feedback from $email";
+            }
+            if (mail($toAddress, $emailSubject, $emailContent, $fromHeader)) {
+                $sent = TRUE;
+            }
+        }
     }
 }
 ?>
@@ -40,13 +47,13 @@ if (isset($name, $email, $feedback)) {
 <body>
 <?php
 if ($sent) {
-    echo "<h1>Feedback Submitted</h1>";
-    echo "<p>Your feedback has been sent.</p>";
+    echo '<h1>Feedback Submitted</h1>';
+    echo '<p>Your feedback has been sent.</p>';
     echo nl2br(htmlentities($emailContent));
 }
 else {
-    echo "<h1>Feedback Not Submitted</h1>";
-    echo "<p>Cannot submit: fields cannot be left blank.";
+    echo '<h1>Feedback Not Submitted</h1>';
+    echo $errorMsg;
 }
 ?>
 </body>
